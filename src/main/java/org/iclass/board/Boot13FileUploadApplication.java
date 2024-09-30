5277fdf05c87407ef0bff7c722dcd9a77323cb57
GAGmanga <gatolindo@naver.com>
2024-09-30 14:05:07 +0900

    1_초기화

---
+++ src/main/java/org/iclass/board/Boot13FileUploadApplication.java
@@ -0,0 +1,13 @@
+package org.iclass.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot13FileUploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot13FileUploadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/iclass/board/Boot13FileUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iclass.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot13FileUploadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
